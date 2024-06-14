f1464f62a72367e75a3663f722fba695bfbe0041
Tawheed Ahmed <tawheedahmed42@outlook.com>
2024-06-14 15:59:53 +0100

    Added admin user and genres to seed.js Added genre data Moved helpers.js

---
+++ db/data/genres.js
@@ -0,0 +1,19 @@
+const genres = [
+    {name: "adventure"},
+    {name: "first person shooter"},
+    {name: "simulation"},
+    {name: "fantasy"},
+    {name: "role playing"},
+    {name: "sports"},
+    {name: "strategy"},
+    {name: "puzzle"},
+    {name: "racing"},
+    {name: "action"},
+    {name: "horror"},
+    {name: "single player"},
+    {name: "multiplayer"},
+    {name: "moba"},
+    {name: "survival"}
+]
+
+export default genres
--- db/seed.js
+++ db/seed.js
@@ -1,5 +1,35 @@
+import mongoose from 'mongoose';
 import gamesData from './data/games.js'
+import User from '../models/user.js';
+import Genre from "../models/genre.js"
+import { dbURI } from '../config/environment.js'
+import genresData from './data/genres.js';
 
+async function seed() {
+    await mongoose.connect(dbURI)
+    console.log("Connected!")
 
+    mongoose.connection.db.dropDatabase()
 
-console.log(gamesData);
+    const user = await User.create({
+        username: "admin",
+        password: "admin",
+        email: "admin@admin.com",
+        isAdmin: true
+    })
+
+    const genres = await Genre.create(genresData)
+
+    gamesData.forEach((game) => {
+        game.addedBy = user._id
+    })
+
+    console.log(gamesData)
+
+    mongoose.disconnect()
+    console.log("Disconnected!")
+}
+
+seed()
+
+// console.log(gamesData);
