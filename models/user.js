b179a2fa28bbe2267700ad91af79d7f1e5bd9541
Tawheed Ahmed <tawheedahmed42@outlook.com>
2024-06-14 11:26:46 +0100

    Added user model
    - Keys: username, email, password, isAdmin
    - Regexp testing for email (if it contains @ and .)
    - bcrypt encrypts password before saving it in the database (password saved as already hashed)

--- models/user.js
+++ models/user.js
@@ -1,1 +1,25 @@
-console.log("hi")
+import mongoose, { Mongoose } from "mongoose";
+import mongooseUniqueValidator from "mongoose-unique-validator";
+import mongooseHidden from "mongoose-hidden"
+import bcrypt from "bcrypt"
+
+const hidden = mongooseHidden({ defaultHidden: {password: true} })
+const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const match = [emailRegex, "Your email address must contain '@'"]
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true, minLength: [5, "Username must be at least 5 characters long."], maxLength: [25, "Username cannot be longer than 25 characters."], unique: [true, "The username {VALUE} is taken."], trim: true},
+    email: {type: String, required: true, unique: true, match: match},
+    password: {type: String, required: true},
+    isAdmin: {type: Boolean, required: true, default: false}
+})
+
+userSchema.pre('save', function(next) {
+    this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync())
+    next()
+})
+
+userSchema.plugin(mongooseUniqueValidator)
+userSchema.plugin(hidden)
+
+export default mongoose.model("User", userSchema)
