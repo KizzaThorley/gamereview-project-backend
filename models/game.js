dc89dee5b2b34b512ab6b82e07c2a9335b5079f7
kizza <kieran.thorley@gmail.com>
2024-06-14 17:21:24 +0100

    genres required true on schema again

--- models/game.js
+++ models/game.js
@@ -15,7 +15,7 @@ const reviewSchema = new mongoose.Schema({
 const gameSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true, unique: [true, "game needs to be unique"] },
     year: { type: Number, required: true, min: [1958, "Your game cant be older than the first game ever"], trim: true },
-    genres: [{type: mongoose.Schema.ObjectId, required: false, ref: "Genre"}],
+    genres: [{type: mongoose.Schema.ObjectId, required: true, ref: "Genre"}],
     imageUrl: { type: String, required: false, trim: true },
     addedBy: { type: mongoose.Schema.ObjectId, ref: "User", required: true, },
     reviews: [reviewSchema],
