7011d53a772abf5772a179ba5d76fcdfcd174bad
Tawheed Ahmed <tawheedahmed42@outlook.com>
2024-06-14 14:59:23 +0100

    Added genre schema Keys - name Reformatted file structure slightly in models Updated genre key in gameSchema

--- models/game.js
+++ models/game.js
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 import mongooseUniqueValidator from "mongoose-unique-validator";
 import reviewSchema from "./review";
 
-
 const gameSchema = new mongoose.Schema({
     name: { type: String, required: true, trim: true, unique: [true, "game needs to be unique"] },
     year: { type: Number, required: true, min: [1958, "Your game cant be older than the first game ever"], trim: true },
-    genre: { type: String, required: true, trim: true },
+    genres: [{type: mongoose.Schema.ObjectId, required: true, ref: "Genre"}],
     imageUrl: { type: String, required: false, trim: true },
     addedBy: { type: mongoose.Schema.ObjectId, ref: "User", required: true, },
     reviews: [reviewSchema],
 })
+
 
 gameSchema.plugin(mongooseUniqueValidator)
 
