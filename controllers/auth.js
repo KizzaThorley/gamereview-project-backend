9625cd6e3a2cc7a3c9059aa31ccf0e322f41bb99
kizza <kieran.thorley@gmail.com>
2024-06-15 13:19:54 +0100

    - added login post request

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import User from '../models/user.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { secret } from '../config/environment.js'
 
 const router = express.Router()
 
-router.get('/signup', async (req, res) => {
+router.post('/signup', async (req, res) => {
     try {
         if (req.body.password !== req.body.passwordConfirmation) {
             throw new Error('passwords dont match')
@@ -16,7 +19,7 @@ router.get('/signup', async (req, res) => {
         if (!passwordStrength) {
             throw new Error('Your Password need to have a captial be 8 characters long with a special character')
         }
-        
+
         if (!req.body.email) {
             throw new Error('an email is required')
         }
@@ -27,7 +30,45 @@ router.get('/signup', async (req, res) => {
         res.status(201).json({
             message: `Thank you for signing up ${user.username}`
         })
-        
+
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+
+router.post('/login', async (req, res) => {
+    try {
+        const user = await User.findOne({ email: req.body.email })
+
+        if (!user) {
+            throw new Error("Unauthorized")
+        }
+
+        const passwordCheck = await bcrypt.compare(
+            req.body.password,
+            user.password
+        )
+        if (!passwordCheck) {
+            throw new Error("Unauthorized")
+        }
+
+        const token = jwt.sign(
+            {
+                username: user.username,
+                userId: user._id,
+            },
+            secret,
+            {
+                expiresIn: '14d',
+            }
+        )
+
+        res.send({
+            message: 'hi',
+            token: token
+        })
+
     } catch (error) {
         res.send(error.message)
     }
