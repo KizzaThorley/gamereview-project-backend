070c267d2f803c6ae2ed0d25aa0cfb25b9cdd89e
kizza <kieran.thorley@gmail.com>
2024-06-17 18:45:22 +0100

    added delete and put request for games
    - created put request for games
    - created delete request for games

--- controllers/games.js
+++ controllers/games.js
@@ -1,11 +1,9 @@
 import express from 'express'
 import Game from '../models/game.js'
 import Genre from '../models/genre.js'
-import secureRoute from  '../middleware/secureRoute.js'
-import genresData from "../db/data/genres.js"
-import user from '../models/user.js'
-import gameGenreMapping from '../db/data/gameGenreMapping.js'
-import { ExistingGame, GenreNotFound } from '../lib/errors.js'
+import secureRoute from '../middleware/secureRoute.js'
+import User from '../models/user.js'
+import { ExistingGame, GenreNotFound, NotFound, Unauthorized } from '../lib/errors.js'
 import genre from '../models/genre.js'
 
 const router = express.Router()
@@ -13,9 +11,10 @@ const router = express.Router()
 router.get('/games', async (req, res, next) => {
     try {
 
+
         const games = await Game.find().populate("genres")
         res.send(games)
-        
+
     } catch (error) {
         next(error)
     }
@@ -31,29 +30,92 @@ router.post("/games", secureRoute, async (req, res, next) => {
         }
 
 
-        const genreIds = await Promise.all(req.body.genres.map( async (genre) => {
-                const fullGenre = await Genre.findOne({name: genre.toLocaleLowerCase()})
-                if (!fullGenre) {
-                    throw new GenreNotFound()
-                }
-                return fullGenre
+        const genreIds = await Promise.all(req.body.genres.map(async (genre) => {
+            const fullGenre = await Genre.findOne({ name: genre.toLocaleLowerCase() })
+            if (!fullGenre) {
+                throw new GenreNotFound()
+            }
+            return fullGenre
         }));
 
         console.log(genreIds)
         req.body.genres = genreIds
-       
-        
+
+
         req.body.name = req.body.name.toLocaleLowerCase()
         req.body.addedBy = res.locals.currentUser
         const createdGame = await Game.create(req.body)
 
         return res.status(201).json(createdGame)
-        
+
     } catch (error) {
         next(error)
     }
 })
 
+
+router.delete('/games/:gameId', secureRoute, async (req, res, next) => {
+    console.log(res.locals.currentUser._id);
+    try {
+        const gameToDelete = await Game.findById(req.params.gameId)
+
+        if (!gameToDelete) throw new NotFound()
+
+        const currentUser = await User.findById(res.locals.currentUser._id.toString())
+
+        if (currentUser.isAdmin) {
+            await gameToDelete.deleteOne()
+            return res.status(204).end()
+        }
+        if (!gameToDelete.addedBy.equals(res.locals.currentUser._id)) {
+            throw new Unauthorized()
+        }
+
+        await gameToDelete.deleteOne()
+
+        return res.status(204).end()
+    } catch (error) {
+        next(error)
+    }
+
+})
+
+router.put('/games/:gameId', secureRoute, async (req, res, next) => {
+    try {
+        const gameToUpdate = await Game.findById(req.params.gameId)
+
+
+        if (!gameToUpdate) throw new NotFound()
+            const currentUser = await User.findById(res.locals.currentUser._id.toString())
+
+        if (currentUser.isAdmin) {
+            await gameToDelete.deleteOne()
+            return res.status(204).end()
+        }
+        if (!gameToUpdate.addedBy.equals(res.locals.currentUser._id)) {
+            throw new Unauthorized()
+        }
+
+        const genreIds = await Promise.all(req.body.genres.map(async (genre) => {
+            const fullGenre = await Genre.findOne({ name: genre.toLocaleLowerCase() })
+            if (!fullGenre) {
+                throw new GenreNotFound()
+            }
+            return fullGenre
+        }));
+
+        req.body.genres = genreIds
+
+        Object.assign(gameToUpdate, req.body);
+        await gameToUpdate.save();
+
+        return res.status(202).json(gameToUpdate)
+
+    } catch (error) {
+        next(error)
+    }
+
+})
 
 
 
